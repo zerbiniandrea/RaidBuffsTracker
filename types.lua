cc86c8bfc4af47c3e899b4ecc7d80a01542c842a
zerby <zerby.programming@proton.me>
2026-01-25 20:46:34 +0100

    refactor: ♻️ move type definitions to separate types.lua file

---
+++ types.lua
@@ -0,0 +1,73 @@
+---@meta
+-- Type definitions for LuaLS (lua-language-server)
+-- This file is NOT loaded by WoW - it's only used for type checking during development
+
+-- WoW API types (stubs)
+---@class Frame
+---@class Texture
+---@class FontString
+---@class AnimationGroup
+
+---@alias SpellID number|number[]
+---@alias ClassName "WARRIOR"|"PALADIN"|"HUNTER"|"ROGUE"|"PRIEST"|"DEATHKNIGHT"|"SHAMAN"|"MAGE"|"WARLOCK"|"MONK"|"DRUID"|"DEMONHUNTER"|"EVOKER"
+---@alias RoleType "TANK"|"HEALER"|"DAMAGER"
+
+---@class RaidBuff
+---@field spellID SpellID
+---@field key string
+---@field name string
+---@field class ClassName
+
+---@class PresenceBuff
+---@field spellID SpellID
+---@field key string
+---@field name string
+---@field class ClassName
+---@field missingText string
+---@field infoTooltip? string
+
+---@class PersonalBuff
+---@field spellID SpellID
+---@field key string
+---@field name string
+---@field class ClassName
+---@field missingText string
+---@field groupId? string
+---@field beneficiaryRole? RoleType
+---@field infoTooltip? string
+
+---@class SelfBuff
+---@field spellID SpellID
+---@field key string
+---@field name string
+---@field class ClassName
+---@field missingText string
+---@field groupId? string
+---@field enchantID? number
+---@field buffIdOverride? number
+---@field requiresTalentSpellID? number
+---@field excludeTalentSpellID? number
+---@field iconByRole? table<RoleType, number>
+---@field infoTooltip? string
+
+---@class BuffGroup
+---@field displayName string
+---@field missingText? string
+
+---@class BuffFrame: Frame
+---@field key string
+---@field spellIDs SpellID
+---@field displayName string
+---@field icon Texture
+---@field border Texture
+---@field count FontString
+---@field buffText? FontString
+---@field testText FontString
+---@field isPlayerBuff? boolean
+---@field isPresenceBuff? boolean
+---@field isPersonalBuff? boolean
+---@field isSelfBuff? boolean
+---@field glowTexture? Texture
+---@field glowAnim? AnimationGroup
+---@field glowShowing? boolean
+---@field currentGlowStyle? number
